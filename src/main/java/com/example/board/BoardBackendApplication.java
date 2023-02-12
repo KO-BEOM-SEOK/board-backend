1da55c330651799347a9028d75676b044cd3c5c1
HOON <singeresman@naver.com>
2023-02-12 17:36:22 +0900

    feat : 암호화 Annotation 추가

--- src/main/java/com/example/board/BoardBackendApplication.java
+++ src/main/java/com/example/board/BoardBackendApplication.java
@@ -3,7 +3,10 @@ package com.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BoardBackendApplication {
 
 	public static void main(String[] args) {
